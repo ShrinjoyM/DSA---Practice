babd267300c164d4628361deceb9bb724260ba96
ShrinjoyM <shrinjoymudi@gmail.com>
2025-11-16 00:32:36 +0530

    Bitwise Operator-Day5.1

---
+++ BITWISE OPERATOR/and.cpp
@@ -0,0 +1,8 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int a=5; // 0101
+    int b=3; // 0011
+    cout<<"a&b = "<<(a & b); // 0001 = 1
+    return 0;
+}
---
+++ BITWISE OPERATOR/not.cpp
@@ -0,0 +1,9 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int a=5; // 0101
+    int b=3; // 0011
+    cout<<"~a = "<<~a<<endl; // 1110 = -6
+    cout<<"~b = "<<~b; // 1100 = -4
+    return 0;
+}
