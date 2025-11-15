cfd48b821461b537e977b646d3ca161366d9f35d
ShrinjoyM <shrinjoymudi@gmail.com>
2025-11-16 00:59:02 +0530

    Shifting-Day5.3

---
+++ BITWISE OPERATOR/leftshift.cpp
@@ -0,0 +1,9 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int a=19; // 10011
+    int b=21; // 10101
+    cout<<"19<<1 = "<<(a<<1)<<endl; // 100110 = 38
+    cout<<"19<<2 = "<<(b<<2); // 1010100 = 84
+    return 0;
+}
---
+++ BITWISE OPERATOR/rightshift.cpp
@@ -0,0 +1,8 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int a=17; // 10001
+    cout<<"17>>1 = "<<(a>>1)<<endl; // 01000 = 8
+    cout<<"17>>2 = "<<(a>>2); // 00100 = 4
+    return 0;
+}
