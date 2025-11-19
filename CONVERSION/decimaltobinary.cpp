bc36d12603e59a6987ba3d8533a2dfa4426e4319
ShrinjoyM <shrinjoymudi@gmail.com>
2025-11-19 20:34:09 +0530

    Conversion-Day8.2

--- CONVERSION/decimaltobinary.cpp
+++ CONVERSION/decimaltobinary.cpp
@@ -1,29 +1,20 @@
 #include<bits/stdc++.h>
 using namespace std;
 int decimaltobinary(int n){
-    int bit,ans=0,i=1;
-    while (n!=0) {
-    bit = n % 2;
-    n /= 2;
-    ans+= bit * i;
-    i *= 10;
-  }
+    float ans=0,i=0,bit;
+    while(n!=0){
+        bit=n%2;
+        ans+=bit*(pow(10,i));
+        n/=2;
+        i++;
+    }
     return ans;
 }
 int main(){
     int n;
-    cout<<"Enter the decimal number: ";
+    cout<<"Enter the decimal n.: ";
     cin>>n;
-    if(n>0){
-        cout<<decimaltobinary(n);
-    }
-    else{
-        n=;
-        int ans=decimaltobinary(n);
-        int newAns=ans+1;
-        // 2's comp
-        //newAns = newAns+1;
-        cout << newAns << endl;
-    }
-    return 0;   
+    int ans=decimaltobinary(n);
+    cout<<ans;
+    return 0;
 }
