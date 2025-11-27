a55911a43bd1bf78b4ef98277fc9f6456a15fc38
ShrinjoyM <shrinjoymudi@gmail.com>
2025-11-27 12:35:03 +0530

    Arrays-Day12.12

---
+++ ARRAYS/init.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int ar[10];
+    for(int i=0;i<10;i++){
+        ar[i]=3;
+    }
+    for(int i=0;i<10;i++){
+        cout<<ar[i]<<"\t";
+    }
+}
