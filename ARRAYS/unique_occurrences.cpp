f1c71f4e507ff47f47d6663e3d2e8ec989cde592
ShrinjoyM <shrinjoymudi@gmail.com>
2025-12-01 01:03:08 +0530

    Arrays-Day15.2

--- ARRAYS/unique_occurrences.cpp
+++ ARRAYS/unique_occurrences.cpp
@@ -1,44 +1,45 @@
 #include<bits/stdc++.h>
 using namespace std;
-bool occur(int ar[],int n){
-    int i,j,c=1,arr[100],k=0,arr1[100],x=0,b,count=0;
+bool occur(int arr[],int n){
+    int i,j,c=0,arr2[100],k=0,arr1[100],x=0,b,count=0;
     for(i=0;i<n;i++){
         if(i==0){
-            arr1[x]=ar[i];
+            arr1[x]=arr[i];
             x++;
-            for(j=i+1;j<n;j++){
-                if(ar[i]==ar[j]){
+            for(j=0;j<n;j++){
+                if(arr[i]==arr[j]){
                     c++;
                 }
             }
         }
         else{
             for(b=0;b<x;b++){
-                if(ar[i]==arr1[b]){
+                if(arr[i]==arr1[b]){
                     count++;
                 }
             }
             if(count==0){
-                arr1[x]=ar[i];
+                arr1[x]=arr[i];
                 x++;
-                for(j=i+1;j<n;j++){
-                    if(ar[i]==ar[j]){
+                for(j=0;j<n;j++){
+                    if(arr[i]==arr[j]){
                         c++;
                     }
                 }
             }
             count=0;
         }
-        if(c>1){
-            arr[k]=c;
+        if(c>0){
+            arr2[k]=c;
+            cout<<arr2[k]<<" ";
             k++;
-            c=1;
+            c=0;
         }
     }
     bool unq=1;
     for(int i=0;i<k;i++){
         for(int j=i+1;j<k;j++){
-            if(arr[i]==arr[j]){
+            if(arr2[i]==arr2[j]){
                 unq=0;
                 break;
             }
