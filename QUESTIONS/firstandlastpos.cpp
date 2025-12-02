3f11e847533bbfabc19e576d96093fca1f59fcde
ShrinjoyM <shrinjoymudi@gmail.com>
2025-12-03 02:20:56 +0530

    Questions-Day17.2

--- QUESTIONS/firstandlastpos.cpp
+++ QUESTIONS/firstandlastpos.cpp
@@ -1,60 +1,61 @@
 #include <bits/stdc++.h>
 using namespace std;
-int totalOcc(int ar[],int n,int key){
-    int st=0,end=n-1,f=-1,l=-1,occ;
-    while (st<=end){
+int firstPos(vector<pair<int,int>> &ar,int n,int key){
+    int st=0,end=n-1,f=-1;
+    while(st<=end){
         int mid=st+(end-st)/2;
-        if (ar[mid]==key){
-            f=mid;
+        if(ar[mid].first==key){
+            f=ar[mid].second+1;
             end=mid-1;
         }
-        else if (ar[mid]<key){
+        else if(ar[mid].first<key){
             st=mid+1;
         }
         else{
             end=mid-1;
         }
     }
-    st=0,end=n-1;
-    while (st<=end){
+    return f;
+}
+int lastPos(vector<pair<int,int>> &ar,int n,int key){
+    int st=0,end=n-1,l=-1;
+    while(st<=end){
         int mid=st+(end-st)/2;
-        if (ar[mid]==key){
-            l=mid;
+        if (ar[mid].first==key){
+            l=ar[mid].second+1;
             st=mid+1;
         }
-        else if (ar[mid]<key){
+        else if(ar[mid].first<key){
             st=mid+1;
         }
         else{
             end=mid-1;
         }
     }
-    if(f==-1 && l==-1){
-        occ=-1;
-    }
-    else{
-        occ=(l-f)+1;
-    }
-    return occ;
+    return l;
 }
 int main(){
     int size;
     cout<<"Enter the size of the array: ";
     cin>>size;
-    int ar[100];
+    vector<pair<int,int>> ar;
     cout<<"Enter the array elements: "<<endl;
     for(int i=0;i<size;i++){
-        cin>>ar[i];
+        int x;
+        cin>>x;
+        ar.push_back({x,i});
     }
+    sort(ar.begin(),ar.end());
     int key;
-    cout<<"Enter the element whose occurrence to be checked: ";
+    cout<<"Enter the element: ";
     cin>>key;
-    int occ=totalOcc(ar,size,key);
-    if(occ==-1){
+    int f=firstPos(ar,size,key);
+    int l=lastPos(ar,size,key);
+    if(f==-1 && l==-1){
         cout<<"Element not present.";
     }
     else{
-        cout<<"Occurrence of "<<key<<" = "<<occ;
+        cout<<"First Position = "<<f<<endl<<"Last Position = "<<l;
     }
     return 0;
 }
