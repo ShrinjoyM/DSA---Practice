be11a37c342a96c77f1e93863e444f55056375d8
ShrinjoyM <shrinjoymudi@gmail.com>
2025-12-02 15:52:52 +0530

    Binary Search-Day16.1

---
+++ SEARCHING/binary.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+using namespace std;
+int search(int ar[],int n,int key){
+    //sort(ar,ar+n);
+    int st=0,end=n-1;
+    int mid=(st+end)/2,ans=-1;
+    while(st<=end){
+        if(ar[mid]==key){
+            return mid+1;
+            break;
+        }
+        else if(ar[mid]>key){
+            end=mid-1;
+        }
+        else{
+            st=mid+1;
+        }
+        mid=(st+end)/2;
+    }
+    return -1;
+}
+int main(){
+    int size;
+    cout<<"Enter the size of the array: ";
+    cin>>size;
+    int ar[100];
+    cout<<"Enter the array elements in ascending order: "<<endl;
+    for(int i=0;i<size;i++){
+        cin>>ar[i];
+    }
+    int key;
+    cout<<"Enter the element to be searched: ";
+    cin>>key;
+    //cout<<endl;
+    int res=search(ar,size,key);
+    cout<<res;
+    return 0;
+}
