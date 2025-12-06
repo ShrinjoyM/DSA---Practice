951e32cfcb8f31f47cd94d13010a3461d851e1b8
ShrinjoyM <shrinjoymudi@gmail.com>
2025-12-06 19:47:01 +0530

    Search-Day20.3

--- SEARCHING/Questions/bookAllocation.cpp
+++ SEARCHING/Questions/bookAllocation.cpp
@@ -19,7 +19,7 @@ bool isPossible(int ar[],int n,int std,long long mid){
 }
 long long bookAllocation(int ar[],int n,int std){
     if(std>n){
-        return -1;
+        return 0;
     }
     else{
         long long sumPages=0;
